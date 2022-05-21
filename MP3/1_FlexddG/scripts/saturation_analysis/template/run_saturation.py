17c08391d909d5f8c92658a944ee32c6a8d1156b
Aleksandr Kovalenko <68705787+Aleksandr-biochem@users.noreply.github.com>
2022-05-21 12:46:19 +0600

    updated run_saturation.py

--- MP3/1_FlexddG/scripts/saturation_analysis/template/run_saturation.py
+++ MP3/1_FlexddG/scripts/saturation_analysis/template/run_saturation.py
@@ -12,19 +12,17 @@ if use_multiprocessing:
     import multiprocessing
     max_cpus = 20 # We might want to not run on the full number of cores, as Rosetta take about 2 Gb of memory per instance
 
-###################################################################################################################################################################
-# Important: The variables below are set to values that will make the run complete faster (as a tutorial example), but will not give scientifically valid results.
-#            Please change them to the "normal" default values before a real run.
-###################################################################################################################################################################
+# specify path to rosetta
+rosetta_scripts_path = os.path.expanduser("/path/to/rosetta_bin_linux_2021.16.61629_bundle/main/source/bin/rosetta_scripts.mpi.linuxgccrelease")
 
-rosetta_scripts_path = os.path.expanduser("/home/olebedenko/tools/rosetta_bin_linux_2021.16.61629_bundle/main/source/bin/rosetta_scripts.mpi.linuxgccrelease")
+# Flex ddG parameters
 nstruct = 35 # Normally 35
 max_minimization_iter = 5000 # Normally 5000
 abs_score_convergence_thresh = 1.0 # Normally 1.0
 number_backrub_trials = 35000 # Normally 35000
 backrub_trajectory_stride = 17500 # Can be whatever you want, if you would like to see results from earlier time points in the backrub trajectory. 7000 is a reasonable number, to give you three checkpoints for a 35000 step run, but you could also set it to 35000 for quickest run time (as the final minimization and packing steps will only need to be run one time).
-path_to_script = 'ddG-backrub.xml'
-residue_to_mutate = ('A', 29, '') # Residue position to perfrom saturation mutatagenesis. Format: (Chain, PDB residue number, insertion code).
+path_to_script = 'ddG-backrub.xml' # path to script for Flex ddG protocol
+residue_to_mutate = ('A', 29, '') # Residue position to perfrom saturation mutatagenesis. Format: (Chain, PDB residue number, insertion code). This one will be substituted further
 
 if not os.path.isfile(rosetta_scripts_path):
     print('ERROR: "rosetta_scripts_path" variable must be set to the location of the "rosetta_scripts" binary executable')
